599c909623d21937f378235abc798a7dabdd8ce7
Michael <michael.spengler@sap.com>
2022-01-18 11:58:34 +0100

    introduce input + button

--- advisor-bot-frontend/src/components/Home.svelte
+++ advisor-bot-frontend/src/components/Home.svelte
@@ -1,5 +1,12 @@
 <script>
   export let name;
+  let inputValue;
+
+  function handleClick() {
+    alert(
+      "das ist ja ein lustiges problem, sei froh dass du keine anderen problemen hast."
+    );
+  }
 </script>
 
 <h1>Hello {name}!</h1>
@@ -7,6 +14,14 @@
   Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how
   to build Svelte apps.
 </p>
+
+<input
+  type="text"
+  bind:value={inputValue}
+  placeholder="geben sie ihr thema ein"
+/>
+
+<button on:click={handleClick}> Send </button>
 
 <link
   href="https://fonts.googleapis.com/css?family=Overpass:100,400"
